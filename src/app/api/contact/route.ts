e2df2ba548b7859358817fcb3c87fe873f4fe543
Eye-Kyu <wankenobi29@gmail.com>
2025-11-09 17:13:33 +0300

    added email template and auto-reply

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,6 +3,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+
 export async function POST(req: Request) {
     try {
         const { name, email, category, message, country } = await req.json();
